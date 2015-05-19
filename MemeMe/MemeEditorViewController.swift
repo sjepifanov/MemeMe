89a27d20314612e1b9e950e8c4fe4f30013be57c
Sergei Jepifanov <sjepifanov@hotmail.com>
2015-05-19 14:18:49 +0300

    Add textField delegate. Keyboard observers.

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,12 +8,146 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController, UITextFieldDelegate {
 
+  // MARK: Outlets
+  @IBOutlet weak var topTextField: UITextField!
+  @IBOutlet weak var bottomTextField: UITextField!
+  @IBOutlet weak var albumButton: UIBarButtonItem!
+  @IBOutlet weak var cameraButton: UIBarButtonItem!
+  @IBOutlet weak var actionButton: UIBarButtonItem!
+  @IBOutlet weak var cancelButton: UIBarButtonItem!
+  
+  //MARK: Declarations
+  let textDelegate = textFieldDelegate()
+  
+  let memeTextAttributes = [
+    NSStrokeColorAttributeName : UIColor.blackColor(),
+    NSForegroundColorAttributeName : UIColor.whiteColor(),
+    NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    NSStrokeWidthAttributeName : -3.0
+  ]
+  
+  //MARK: View
   override func viewDidLoad() {
     super.viewDidLoad()
 
   }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+
+    // TODO: Enable when image is pickec from source. May also check if text has been edited in text fields
+    //actionButton.enabled = imageEditorView.image ? true : false
+    
+    // Set textField text attributes.
+    topTextField.defaultTextAttributes = memeTextAttributes
+    bottomTextField.defaultTextAttributes = memeTextAttributes
+    
+    // Set textField delegate
+    topTextField.delegate = textDelegate
+    bottomTextField.delegate = textDelegate
+    
+    // Set textFields text alignment.
+    topTextField.textAlignment = .Center
+    bottomTextField.textAlignment = .Center
+    
+    // Set border style
+    topTextField.borderStyle = .None
+    bottomTextField.borderStyle = .None
+    
+    // Set empty textField text
+    topTextField.text = "TOP"
+    bottomTextField.text = "BOTTOM"
+    
+    subscribeToKeyboardNotifications()
+    
+    
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    unsubscribeFromKeyboardNotifications()
+  }
+  
+  // MARK: Methods
+  
+  // MARK: (Un)Subscribe to UIKeyboardWillShow(Hide)Notification
+  
+  /**
+  Add observer for NSNotificationCenter notifications:
+  
+  UIKeyboardWillShow(Hide)Notification
+  
+  selector: "keyboardWillShow:", "keyboardWillHide:"
+  */
+  func subscribeToKeyboardNotifications() {
+    println("Subscribed")
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+  }
+  
+  /**
+  Remove observer for NSNotificationCenter notifications:
+  
+  UIKeyboardWillShow(Hide)Notification
+  */
+  func unsubscribeFromKeyboardNotifications() {
+    println("Unsubscribed")
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+  }
+
+  
+  // MARK: Selectors for notifications
+  
+  // TODO: Text is covered by Navigation bar in Landscape. May hide toolbar to allow more space.
+  // TODO: If keyboard is on screen and device rotated view did not slide down correctly. Either disallow rotation while editing. Dismiss keyboard or recalculate spece instead of using previous value.
+  /**
+  Slide view frame up by height of on screen keyboard frame.
+  
+  :param: notification: (NSNotification)
+  */
+  func keyboardWillShow(notification: NSNotification) {
+    println("Show: got notification")
+    println(self.view.frame.origin.y)
+    if bottomTextField.isFirstResponder(){
+      view.frame.origin.y -= getKeyboardHeight(notification)
+      println(self.view.frame.origin.y)
+    }
+  }
+  
+  /**
+  Slide view frame down by height of on screen keyboard frame.
+  
+  :param: notification: (NSNotification)
+  
+  */
+  func keyboardWillHide(notification: NSNotification) {
+    println("Hide: got notification")
+    println(self.view.frame.origin.y)
+    view.frame.origin.y = 0.0
+    //if bottomTextField.isFirstResponder(){
+    //  self.view.frame.origin.y += getKeyboardHeight(notification)
+    //  println(self.view.frame.origin.y)
+    //}
+  }
+  
+  /**
+  Return height of on screen keyboard frame when keyboard slides into view.
+  
+  :param: notification: (NSNotification)
+  
+  :returns: CGFloat Height of on screen keyboard frame.
+  */
+  func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+    let userInfo = notification.userInfo
+    let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue //of CGRect
+    return keyboardSize.CGRectValue().height
+  }
+
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
---
+++ MemeMe/textFieldDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  textFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Sergei on 19/05/15.
+//  Copyright (c) 2015 Sergei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class textFieldDelegate: NSObject,  UITextFieldDelegate {
+  
+
+  func textFieldDidBeginEditing(textField: UITextField) {
+    println("begin editing: \(textField.tag)")
+    // Clear textField of preset text.
+    if textField.text == "TOP" || textField.text == "BOTTOM" {
+      textField.text = ""
+    }
+  }
+  
+  func textFieldDidEndEditing(textField: UITextField) {
+    // Set preset text if text field is empty. topTextField tag is 0 bottomTextField tag is 1
+    if textField.text.isEmpty && textField.tag == 0 {
+      textField.text = "TOP"
+    } else {
+      textField.text = "BOTTOM"
+    }
+  }
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    // Force all characters to be uppercase, no matter what.
+    let lowercaseCharacters = NSCharacterSet.lowercaseLetterCharacterSet()
+    if let lowercaseRange = string.rangeOfCharacterFromSet(lowercaseCharacters) {
+      let uppercaseString = string.uppercaseString
+      if textField.text.isEmpty {
+        // Updates return button; forces cursor to the end
+        textField.text = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: uppercaseString)
+      } else {
+        // Preserves cursor location; doesn't update return button
+        let beginning = textField.beginningOfDocument
+        let start = textField.positionFromPosition(beginning, offset: range.location)!
+        let end = textField.positionFromPosition(start, offset: range.length)!
+        let range = textField.textRangeFromPosition(start, toPosition: end)
+        textField.replaceRange(range, withText: uppercaseString)
+      }
+      return false
+    } else {
+      return true
+    }
+  }
+
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    
+    return true;
+  }
+
+  
+}
