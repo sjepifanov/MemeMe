e4d8d6c89c0a510a0d6c42617629b63ea759511d
Sergei Jepifanov <sjepifanov@hotmail.com>
2015-05-29 18:44:26 +0300

    Cleaning the code

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
 
   // MARK: Outlets
+  
   @IBOutlet weak var topTextField: UITextField!
   @IBOutlet weak var bottomTextField: UITextField!
   @IBOutlet weak var albumButton: UIBarButtonItem!
@@ -18,14 +19,15 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   @IBOutlet weak var actionButton: UIBarButtonItem!
   @IBOutlet weak var cancelButton: UIBarButtonItem!
   @IBOutlet weak var scrollView: UIScrollView!
-  @IBOutlet weak var contentView: UIView!
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var toolBar: UIToolbar!
   
   //MARK: Declarations
   
+  // Define appDelegate to hold shared data structure.
   let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
   
+  // Data object from Meme Detail View.
   var meme: Meme!
   
   let textDelegate = textFieldDelegate()
@@ -37,10 +39,11 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     NSStrokeWidthAttributeName : -3.0
   ]
   
+  
   //MARK: View
   override func viewDidLoad() {
     super.viewDidLoad()
-    println("Editor ViewDidLoad")
+
     cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
     
     // Set textField text attributes.
@@ -65,6 +68,8 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     
     scrollView.delegate = self
     scrollView.setTranslatesAutoresizingMaskIntoConstraints(false)
+    // Set maximum zoom scale to image original size
+    scrollView.maximumZoomScale = 1
     
     // Enable user interaction to recognize touches and gestures
     imageView.userInteractionEnabled = true
@@ -80,13 +85,14 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
-
-    println("Editor ViewWillAppear")
     
     if let _ = meme {
       imageView.image = meme.originalImage
       topTextField.text = meme.topText
       bottomTextField.text = meme.bottomText
+      setMinimumZoomForCurrentFrame(meme.originalImage.size)
+      setMaxZoomToPresentPicture(meme.originalImage.size)
+      centerScrollViewContents()
     }
     
     // Enable action button when picture is loaded.
@@ -96,8 +102,8 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   }
   
   override func viewWillDisappear(animated: Bool) {
-    println("Editor ViewWillDisappear")
     super.viewWillDisappear(animated)
+    
     unsubscribeFromKeyboardNotifications()
   }
   
@@ -126,7 +132,6 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     
     actionController.completionWithItemsHandler = {action, result, object, error in
       if result{
-        println("completed action \(action)")
         self.save()
         
         var storyboard = UIStoryboard (name: "Main", bundle: nil)
@@ -136,9 +141,6 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         if let navigationcontroller = self.navigationController {
           navigationcontroller.pushViewController(tableViewController, animated: true)
         }
-        
-      }else{
-        println("something wrong: \(action)")
       }
     }
   }
@@ -165,7 +167,6 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   selector: "keyboardWillShow:", "keyboardWillHide:"
   */
   func subscribeToKeyboardNotifications() {
-    println("Subscribed")
     NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
     NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
   }
@@ -176,7 +177,6 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   UIKeyboardWillShow(Hide)Notification
   */
   func unsubscribeFromKeyboardNotifications() {
-    println("Unsubscribed")
     NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
     NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
   }
@@ -191,28 +191,19 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
   :param: notification: (NSNotification)
   */
   func keyboardWillShow(notification: NSNotification) {
-    println("Show: got notification")
     
     if bottomTextField.isFirstResponder(){
-      
-      // TODO: Hide Bars. Doing test now. Not looking good the view did not slide up. Though bars did disappear. The problem is with Navbar called through Nav Controller. will try to call it through proprty. Seems like it redraw the view moving origin to y. Should place text differently instead.
-      
       view.frame.origin.y -= getKeyboardHeight(notification)
-      println(self.view.frame.origin.y)
-      
     }
   }
   
   /**
-  Slide view frame down by height of on screen keyboard frame.
+  Slide view frame down when keyboard is off screen.
   
   :param: notification: (NSNotification)
   
   */
   func keyboardWillHide(notification: NSNotification) {
-    println("Hide: got notification")
-    
-    // That seems like proper way to get rid of view slide on rotation.
     view.frame.origin.y = 0.0
   }
   
@@ -229,92 +220,113 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     return keyboardSize.CGRectValue().height
   }
   
-  // MARK: Image Picker Controller Delegates methods
+  // MARK: Image Picker Controller Delegates methods.
   
   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-    println("imagePicker delegate")
+
     if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
       
-      // Dismiss UIImagePickerController when selection is made
+      // Dismiss UIImagePickerController when selection is made.
       dismissViewControllerAnimated(true, completion: nil)
       
       imageView.image = image
-      println("imageView frame: \(imageView.frame.size)")
-      println("contentView frame: \(contentView.frame.size)")
       
-      // TODO: Check more articles on Autolayout with Scroll View. So far it's just not working right in landscape. Picture frame is cut.
-      scrollView.contentSize = image.size
-      println("scrollView contentSize: \(scrollView.contentSize)")
-      println("scrollView frame: \(scrollView.frame.size)")
-      let scrollViewFrame = scrollView.frame
-      let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
-      let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+      imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height)
       
-      // TODO: Should play with Zoom Scale to present picture taking whole screen and as nice as possible
-      // take care of inset whenstart showing do programmable zoom otherwise image did not scroll
-      // check frame. Think if you need to hide bars. Not alsways pretty.
-      
-      let minScale = min(scaleWidth, scaleHeight)
-      //let minScale = max(scaleWidth, scaleHeight)
-      
-      //println(minScale)
-      
-      scrollView.minimumZoomScale = minScale
-      
+      // Set minimum zoom scale.
       scrollView.maximumZoomScale = 1
-      scrollView.zoomScale = minScale
-
+      setMinimumZoomForCurrentFrame(imageView.frame.size)
+      scrollView.zoomScale = scrollView.minimumZoomScale
       
-      println("imageView frame: \(imageView.frame.size)")
-      println("contentView frame: \(contentView.frame.size)")
-      println("scrollView frame: \(scrollView.frame.size)")
+      // Set zoom scale so image will appear fullscreen.
+      setMaxZoomToPresentPicture(image.size)
       
+      // Move Scroll view to center point.
+      scrollView.setContentOffset(CGPoint(x: scrollView.frame.size.width / 2, y: scrollView.frame.size.height / 2) , animated: true)
+      
+      // Center content
       centerScrollViewContents()
     }
   }
   
   func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-    // Dismiss on cancel
-    println("imagePicker dismissed")
     dismissViewControllerAnimated(true, completion: nil)
   }
   
   // MARK: Scroll View Delegates and Methods
   
   func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-    println("viewForZooming")
-    //return imageView
-    return contentView
+    return imageView
   }
   
+  
+  /**
+  Set minimum Zoom Level for current frame.
+  */
+  func setMinimumZoomForCurrentFrame(imageViewFrameSize: CGSize){
+    let imageSize = imageViewFrameSize
+    let scrollSize = scrollView.bounds.size
+    let scaleWidth = scrollSize.width / imageSize.width
+    let scaleHeight = scrollSize.height / imageSize.height
+    let minScale = min(scaleWidth, scaleHeight)
+    scrollView.minimumZoomScale = minScale
+  }
+  
+  /**
+  Set Zoom Level for current frame so image fill the screen.
+  */
+  func setMaxZoomToPresentPicture(imageSize: CGSize) {
+    let scrollSize = scrollView.bounds.size
+    let imageSize = imageSize
+    let scaleWidth = scrollSize.width / imageSize.width
+    let scaleHeight = scrollSize.height / imageSize.height
+    let maxScale = max(scaleWidth, scaleHeight)
+
+    scrollView.zoomScale = maxScale
+  }
+  
+  
   func scrollViewDidZoom(scrollView: UIScrollView) {
-    println("did Zoom")
     centerScrollViewContents()
   }
   
+  func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
+    
+    // If image is smaller than the screen make scrollView insets to move the image around screen
+    var screenWidth = UIScreen.mainScreen().bounds.size.width
+    var screenHeight = UIScreen.mainScreen().bounds.size.height
+    var viewWidth = imageView.frame.size.width
+    var viewHeight = imageView.frame.size.height
+
+    var x: CGFloat = 0
+    var y: CGFloat = 0
+    
+    if(viewWidth < screenWidth) {
+      x = screenWidth / 2
+    }
+    if(viewHeight < screenHeight) {
+      y = screenHeight / 2
+    }
+    
+    self.scrollView.contentInset = UIEdgeInsetsMake(y, x, y, x);
+  }
+  
+  /**
+  Center scrollView content when it's smaller than screen size
+  */
   func centerScrollViewContents() {
+    let excessiveWidth = max(0.0, scrollView.bounds.size.width - scrollView.contentSize.width)
+    let excessiveHeight = max(0.0, scrollView.bounds.size.height - scrollView.contentSize.height)
+    let insetX = excessiveWidth / 2.0
+    let insetY = excessiveHeight / 2.0
     
-    let boundsSize = scrollView.bounds.size
-    // Changing to Content View to check the results
-    var contentsFrame = contentView.frame
-    //var contentsFrame = imageView.frame
-    
-    if contentsFrame.size.width < boundsSize.width {
-      
-      contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2
-    } else {
-      contentsFrame.origin.x = 0
-    }
-    
-    if contentsFrame.size.height < boundsSize.height {
-      contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2
-    } else {
-      contentsFrame.origin.y = 0
-    }
-    
-    contentView.frame = contentsFrame
-    //imageView.frame = contentsFrame
-    
+    scrollView.contentInset = UIEdgeInsetsMake(
+    max(insetY, 0.0),
+    max(insetX, 0.0),
+    max(insetY, 0.0),
+    max(insetX, 0.0)
+    )
+
   }
   
   // MARK: Creating and saving Meme methods
